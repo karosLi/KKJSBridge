7cf9343838c77fb87da206dd2499eebc14aefee1
Karosli <karosli1314@gmail.com>
2020-07-10 01:19:48 +0800

    修改导入方式

---
+++ KKJSBridgeDemo/Pods/Headers/Private/KKJSBridge/KKJSBridgeModuleXMLHttpRequestDispatcher.h
@@ -0,0 +1,1 @@
+../../../../../KKJSBridge/KKJSBridge/Modules/Ajax/AjaxHook/KKJSBridgeModuleXMLHttpRequestDispatcher.h
---
+++ KKJSBridgeDemo/Pods/Headers/Public/KKJSBridge/KKJSBridgeXMLHttpRequest.h
@@ -0,0 +1,1 @@
+../../../../../KKJSBridge/KKJSBridge/Modules/Ajax/AjaxHook/KKJSBridgeXMLHttpRequest.h
