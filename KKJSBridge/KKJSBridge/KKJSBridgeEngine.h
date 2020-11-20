77330c44a952e9a4b9bae70fac06343a1eb0b9fa
Karosli <karosli1314@gmail.com>
2020-11-20 23:52:14 +0800

    增加注释：如果使用的不是 KKWebView 而且自定义的 WKWebView，而又想要处理同步 JSBridge 调用，可以按下面方式来处理 JSBridge 同步调用。

--- KKJSBridge/KKJSBridge/KKJSBridgeEngine.h
+++ KKJSBridge/KKJSBridge/KKJSBridgeEngine.h
@@ -43,7 +43,19 @@ typedef void (^KKJSBridgeReadyCallback)(KKJSBridgeEngine *engine);
 - (void)dispatchEvent:(NSString *)eventName data:(NSDictionary * _Nullable)data;
 
 /**
- 分发一个调用。这个分发可以是来自 H5，也可以是来自 Native
+ 分发一个调用。这个分发可以是来自 H5，也可以是来自 Native。
+ 
+ 讨论：
+ 如果使用的不是 KKWebView 而且自定义的 WKWebView，而又想要处理同步 JSBridge 调用，可以按下面方式来处理 JSBridge 同步调用。
+ 
+ #import <KKJSBridge/KKJSBridge.h>
+ 
+ - (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * _Nullable result))completionHandler {
+     // 处理来自 KKJSBridge 的同步调用
+     if ([self handleSyncCallWithPrompt:prompt defaultText:defaultText completionHandler:completionHandler]) {
+         return;
+     }
+ }
 
  @param module 模块名
  @param method 方法名
